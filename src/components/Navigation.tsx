ef8916ac400779a58890b185ab1484ef7d7be6ec
Thalys93 <luisthalys1@hotmail.com>
2025-06-16 14:14:31 -0300

    fix: adjust in navigation name

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -53,7 +53,7 @@ const Navigation = () => {
         <div className="flex items-center justify-between h-16">          
           <div className="flex-shrink-0">
             <span className="text-2xl font-bold font-poppins text-gradient">
-              Portfolio
+              Thalys Dev
             </span>
           </div>
 
